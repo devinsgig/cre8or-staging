4688c94c54ddef1154267bc950869839cdfbc080
devinsgig <165755642+devinsgig@users.noreply.github.com>
2025-09-01 03:22:17 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Packages\ShaunSocial\Core\Http\Controllers\Api\AppController;
 
-Route::get('/{any}', function () {
-    return view('shaun_core::app');
-})->where('any', '^(?!api).*$');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Here is where you can register web routes for your application.
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+// Main SPA catch-all (keeps frontend working)
+Route::get('/{any}', function () {
+    return view('shaun_core::app');
+})->where('any', '^(?!api).*$');
+
+// Ensure /api/app exists even if routes/api.php isn't loaded
+Route::prefix('api')->group(function () {
+    // Temporary health route to prove path works
+    Route::get('/app-ping', function () {
+        return response()->json(['ok' => true, 'ts' => now()->toIso8601String()]);
+    });
+
+    // Real endpoint the SPA expects
+    Route::get('/app', [AppController::class, 'index']);
+    Route::get('/v1/app', [AppController::class, 'index']);
+});
